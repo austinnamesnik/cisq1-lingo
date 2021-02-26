5e930955d34ae6d3736ddd49b7295eff09baffbe
austinnamesnik <austinnamesnik@gmail.com>
2021-02-26 22:11:00 +0100

    Redo of the InvalidFeedbackException

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/exception/InvalidFeedbackException.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/exception/InvalidFeedbackException.java
@@ -1,5 +1,7 @@
 package nl.hu.cisq1.lingo.trainer.domain.exception;
 
 public class InvalidFeedbackException extends RuntimeException {
-
+    public InvalidFeedbackException() {
+        super("The amount of marks is not the same as the length of the word");
+    }
 }

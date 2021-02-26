77af98b0a4465f92af84d653a3fd62c4ba7e3d1b
austinnamesnik <austinnamesnik@gmail.com>
2021-02-26 22:04:14 +0100

    Second try on Java 11 without sun.tools.javac.util

--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
@@ -1,8 +1,9 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
-import com.sun.tools.javac.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,28 +12,32 @@ class FeedbackTest {
     @Test
     @DisplayName("The word is guessed when all letters are correct")
     void wordIsGuessed() {
-        Feedback fb = new Feedback("woord", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT));
+        Mark[] marks = {Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT};
+        Feedback fb = new Feedback("woord", Arrays.asList(marks));
         assertTrue(fb.wordIsGuessed());
     }
 
     @Test
     @DisplayName("The word is not guessed when not all letters are correct")
     void wordIsNotGuessed() {
-        Feedback fb = new Feedback("woord", List.of(Mark.PRESENT, Mark.ABSENT, Mark.CORRECT, Mark.CORRECT));
+        Mark[] marks = {Mark.PRESENT, Mark.ABSENT, Mark.CORRECT, Mark.CORRECT};
+        Feedback fb = new Feedback("woord", Arrays.asList(marks));
         assertTrue(fb.wordIsNotGuessed());
     }
 
     @Test
     @DisplayName("The guess is invalid when the guessed word does not have the same amount of letters as the word to guess")
     void guessIsInvalid() {
-        Feedback fb = new Feedback("woord", List.of(Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID));
+        Mark[] marks = {Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID, Mark.INVALID};
+        Feedback fb = new Feedback("woord", Arrays.asList(marks));
         assertTrue(fb.wordIsInvalid());
     }
 
     @Test
     @DisplayName("The guess is not invalid when the guessed word has the same amount of letters as the word to guess")
     void guessIsNotInvalid() {
-        Feedback fb = new Feedback("woord", List.of(Mark.CORRECT, Mark.ABSENT, Mark.ABSENT, Mark.PRESENT, Mark.ABSENT));
+        Mark[] marks = {Mark.CORRECT, Mark.ABSENT, Mark.ABSENT, Mark.PRESENT, Mark.ABSENT};
+        Feedback fb = new Feedback("woord", Arrays.asList(marks));
         assertTrue(fb.wordIsNotInvalid());
     }
 

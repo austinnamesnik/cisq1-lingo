330a83f9cab4c9d48f5d877670113f051aee1e12
austinnamesnik <austinnamesnik@gmail.com>
2021-03-20 23:18:01 +0100

    Removed Code-Smells #4

--- src/main/java/nl/hu/cisq1/lingo/presentation/dto/GameMapper.java
+++ src/main/java/nl/hu/cisq1/lingo/presentation/dto/GameMapper.java
@@ -7,19 +7,18 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface GameMapper {
 
-    @Mapping(target = "game_id", source = "id")
+    @Mapping(target = "gameId", source = "id")
     @Mapping(target = "number", expression = "java(game.getLastRound().getNumber())")
     @Mapping(target = "attempts", expression = "java(game.getLastRound().getAttempts())")
     @Mapping(target = "feedbacks", expression = "java(game.getLastRound().getFeedbackList())")
     @Mapping(target = "hint", expression = "java(game.getLastRound().startRound().getCharacters())")
     GameDTO toGameDTOstart(Game game);
 
-    @Mapping(target = "game_id", source = "id")
+    @Mapping(target = "gameId", source = "id")
     @Mapping(target = "number", expression = "java(game.getLastRound().getNumber())")
     @Mapping(target = "attempts", expression = "java(game.getLastRound().getAttempts())")
     @Mapping(target = "feedbacks", expression = "java(game.getLastRound().getFeedbackList())")
     @Mapping(target = "hint", expression = "java(game.getLastRound().getFeedbackList().get(game.getLastRound().getFeedbackList().size() - 1).giveHint().getCharacters())")
     GameDTO toGameDTOguess(Game game);
-
     CreationDTO toCreationDTO(Game game);
 }
